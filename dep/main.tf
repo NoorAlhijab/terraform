a0f80460654a1474d242f249a96c00e0cc321cfe
NoorAlhijab <wesamnoor@gmail.com>
2024-01-06 20:29:29 -0500

    Initial commit

---
+++ dep/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+  ami = "ami-079db87dc4c10ac91"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "web server"
+  }
+  depends_on = [aws_instance.myec2db]
+
+}
+
+resource "aws_instance" "myec2db" {
+  ami = "ami-079db87dc4c10ac91"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "DB server"
+  }
+
+}
